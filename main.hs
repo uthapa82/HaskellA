10c07ab1a9b5efdfab6d0f5313f1a6fd6b60dbc9
Luke-Kurlandski-TCNJ <44705759+Luke-Kurlandski-TCNJ@users.noreply.github.com>
2021-11-15 16:27:09 -0500

    Update main.hs

--- main.hs
+++ main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Data.List
 
+test = "You are ready to go!"
+
 {- Return list of prime numbers less than or equal to n -}
 primes n =
   [x | x <- [2..n], all (/= 0) [mod x y | y <- [2..x-1]] ]
