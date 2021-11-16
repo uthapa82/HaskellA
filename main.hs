78c5658e6b6b661b915593a673452be5467702b4
Luke-Kurlandski-TCNJ <44705759+Luke-Kurlandski-TCNJ@users.noreply.github.com>
2021-11-16 18:14:04 -0500

    Update main.hs

--- main.hs
+++ main.hs
@@ -9,8 +9,8 @@ Return list of prime numbers less than or equal to n.
   n (Int): integer such that all primes are less than n
   return (Data.List): list of primes
 -}
-primes n =
-  [x | x <- [2..n], all (/= 0) [mod x y | y <- [2..x-1]] ]
+primes n = 
+  -- your code
 
 {- 
 Return a list of pairs of primes less than n such that the 
@@ -27,21 +27,21 @@ Return a list of twin primes less than n.
   n (Int): integer such that all primes are less than n
   return (Data.List): list of tuples of twin primes
 -}
-twinPrimes n =
-  separatedPrimes (\(x,y) -> x + 2 == y) n
+twinPrimes n = 
+  -- your code
 
 {- 
 Return a list of cousin primes less than n.
   n (Int): integer such that all primes are less than n
   return (Data.List): list of tuples of twin primes
 -}
-cousinPrimes n =
-  separatedPrimes (\(x,y) -> x + 4 == y) n
+cousinPrimes n = 
+  -- your code
 
 {- 
 Return a list of sexy primes less than n.
   n (Int): integer such that all primes are less than n
   return (Data.List): list of tuples of twin primes
 -}
-sexyPrimes n =
-  separatedPrimes (\(x,y) -> x + 6 == y) n
+sexyPrimes n = 
+  -- your code
